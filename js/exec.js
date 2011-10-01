d616d3fa2c56a013b86aff56ee344a4dc635b0ad
Troy Grant <Tr5978@gmail.com>
2011-10-01 17:12:14 -0400

    commit
    Signed-off-by: Troy Grant <Tr5978@gmail.com>

---
+++ js/exec.js
@@ -0,0 +1,14 @@
+// js goes her/
+
+var exec = function() {
+	return new exec.prototype.init();
+};
+
+exec.prototype = {
+	init: function() {
+		//init the library	
+	}
+};
+
+exec.prototype.init.prototype = exec.prototype;
+//this is what allows the "factory" technique to work
