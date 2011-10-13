e59382691b93a5e5dc6fba76ba12d889c04b6f5c
Troy Grant <Tr5978@gmail.com>
2011-10-13 16:35:03 -0400

    Changes
    Signed-off-by: Troy Grant <Tr5978@gmail.com>

--- demo/accordion/js/main.js
+++ demo/accordion/js/main.js
@@ -37,16 +37,47 @@ var anim = setInterval(function(){
 
 (function(){
 	
-	exec("#accordion .content").animate({
+	/*exec("#accordion .content").animate({
 		
-		duration: 2000,
+		duration: 1000,
 		easing: "easeOutBounce",
 		props: {
-			height: 200, //we're going to add the px in the lib
-			margin: 20,
-			padding: 50
+			height: 200 //we're going to add the px in the lib
 		}//end of object
 		
+	}); */
+	
+	/*var firstDiv = document.querySelectorAll(".content")[0];
+	
+	firstDiv.style.position = "relative";
+	
+	var height = parseFloat( exec(firstDiv).getStyle("height") );
+	
+	exec(firstDiv).animate({
+		duration: 1000,
+		props: {
+			height: 0,
+			top: -height //you can't change properties like left unless the thing is relative, fixed, or absolute
+		}
+	});*/
+	
+	var overlay = document.getElementById("overlay");
+	
+	overlay.style.display = "block"; //so it can be seen, it's none in the CSS
+	
+	var h = parseFloat(exec(overlay).getStyle("height"));
+	
+	overlay.style.top = -h + "px"; //negative h
+	
+	exec(overlay).animate({
+		props: {top: 0}
 	});
+	
+	overlay.onclick = function(){
+		exec(this).animate({
+			props: {top: -h}
+		});
+		return false;
+	};
 	
 })();
